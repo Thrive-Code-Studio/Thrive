d21aaf6144b81f43adf701e7957d8d7fe1a3e9ba
Elia-Renzoni <elia.renzoni03@gmail.com>
2025-07-19 11:02:59 +0200

    thrive add: set directories architecture and first methods for the single outbound adapter

---
+++ thrive/src/main/java/com/thrivecodestudio/thrive/ThriveApplication.java
@@ -0,0 +1,13 @@
+package com.thrivecodestudio.thrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThriveApplication.class, args);
+	}
+
+}
