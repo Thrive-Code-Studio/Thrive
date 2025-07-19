731d4a9a7be166329e81e281a7b2ba86d26fa19d
elia renzoni <elia.renzoni03@gmail.com>
2025-07-19 16:01:24 +0200

    adder

---
+++ thriveapp/src/main/java/thrivecodestudio/thrive/thriveapp/ThriveappApplication.java
@@ -0,0 +1,13 @@
+package thrivecodestudio.thrive.thriveapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThriveappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThriveappApplication.class, args);
+	}
+
+}
---
+++ thriveapp/src/test/java/thrivecodestudio/thrive/thriveapp/ThriveappApplicationTests.java
@@ -0,0 +1,13 @@
+package thrivecodestudio.thrive.thriveapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThriveappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
