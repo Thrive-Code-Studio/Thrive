e4136d7314a9a06e3e39fbbf8eb7450f2808366e
Elia-Renzoni <elia.renzoni03@gmail.com>
2025-07-21 11:16:38 +0200

    thrive update: set new endpoints

--- thriveapp/src/main/java/thrivecodestudio/thrive/thriveapp/Endpoints/OutboundAdapter.java
+++ thriveapp/src/main/java/thrivecodestudio/thrive/thriveapp/Endpoints/OutboundAdapter.java
@@ -3,6 +3,11 @@ package thrivecodestudio.thrive.thriveapp.Endpoints;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 @RestController
 @RequestMapping(path = "/thrive")
@@ -13,18 +18,48 @@ public class OutboundAdapter {
 
     }
 
-    @PostMapping(path = "/new-therapist-account")
-    public void handleTherapistAccounCreation() {
-
-    }
-
     @PostMapping(path = "/new-patient-account")
     public void handlePatientAccountCreation() {
 
     }
 
-    @PostMapping(path = "/new-relation")
-    public void handlePatientAndTherapistRelation() {
+    @PostMapping(path = "/journal-new-card")
+    public void handleNewJournalCreation(HttpRequest r, HttpResponse w) {
 
-    }   
+    }
+
+    @PostMapping(path = "/task-new-card")
+    public void handleTaskCreation() {
+    
+    }
+
+    @DeleteMapping(path = "/cbt-card-delete/{cardId}")
+    public void handleCBTCardDeletion() {
+    	
+    }
+
+    @DeleteMapping(path= "/cbt-journal-delete/{journalId}")
+    public void handleJournalDeletion() {
+    
+    }
+
+    @DeleteMapping(path = "/task-delete/{taskId}")
+    public void handleTaskDeletion() {
+    }
+
+    @GetMapping(path="/cbt-cards/{userId}")
+    public void handleCBTCardsFetch() {
+    
+    }
+
+    @GetMapping(path = "/journal/{userId}")
+    public void handleJournalsFetch() {
+    
+    }
+
+    @GetMapping(path = "/task/{userId}")
+    public void handleTasksFetch() {
+    
+    }
+
 }
